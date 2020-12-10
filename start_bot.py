8ad24b5af28ccfbabc2d57730dff9ca3542c7877
m-test-user <m.src.drop@gmail.com>
2020-12-10 14:12:41 +0000

    fix tmp dir

--- start_bot.py
+++ start_bot.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import shutil
+import logging
 
 import dropbox
 from dotenv import load_dotenv
@@ -8,6 +9,7 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 
 load_dotenv()
+logging.basicConfig(level = logging.INFO, filename = "log.txt")
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 API_ID = os.getenv('API_ID')
@@ -16,8 +18,7 @@ DBX = os.getenv('DROPBOX_TOKEN')
 
 dbx = dropbox.Dropbox(DBX)
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-tmp_dir = 'tmp'
- 
+tmp_dir = '/tmp_bot'
 
 @bot.on(events.NewMessage(pattern='/start'))
 async def start(event):
@@ -27,17 +28,18 @@ async def start(event):
 
 @bot.on(events.NewMessage)
 async def save_msg(event):
-    
+
     time_stamp = str(datetime.datetime.today())
     output_tmp_name = f'tmp{time_stamp.replace(":", "-")}'
-    
+
     if not os.path.isdir(tmp_dir):
         os.mkdir(tmp_dir)
 
-    with open(f'./tmp/{output_tmp_name}', 'w', encoding='utf8') as tmp_file:
+    with open(f'tmp_bot/{output_tmp_name}', 'w', encoding='utf8') as tmp_file:
         tmp_file.write(f'#inbox #telegram\n---\n\n{event.text}')
 
     send_file_dbx()
+    print('outp')
     shutil.rmtree(tmp_dir)
 
     await event.respond('Отправлено')
@@ -49,7 +51,7 @@ def send_file_dbx():
     list_files = os.listdir(tmp_dir)
 
     for index in list_files:
-        with open(f'./tmp/{index}','rb') as file:
+        with open(f'tmp_bot/{index}','rb') as file:
             file_name = index.replace('tmp', '')
             dbx.files_upload(file.read(), f'/{file_name}.md')
             print('done')
